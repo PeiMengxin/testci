665c884738572173baf5d8ad857e20fb05fc5051
PeiMengxin <pei_mengxin@163.com>
2017-01-10 19:17:00 +0800

    test serial

--- testci.cpp
+++ testci.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-
+#include <serial/serial.h>
 using namespace std;
 
 int main()
 {
     cout<<"testci"<<endl;
+    serial::Serial s;
+    cout<<"baudrate :"<<s.baudrate<<endl;
+    return 0;
 }
