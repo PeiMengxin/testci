910d13c9d60f70ba8eb46a89a7f5aee40774d2b7
PeiMengxin <pei_mengxin@163.com>
2017-01-15 12:08:24 +0800

    add install_deps shell

---
+++ install_deps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ex
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -q
+sudo apt-get install gcc-5 -y
+cd /usr/bin
+sudo rm gcc
+sudo ln -s gcc-5 gcc
+sudo rm g++
+sudo ln -s g++-5 g++
+gcc --version
